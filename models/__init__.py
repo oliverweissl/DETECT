1456094a14bf0d1482336ad84c8151039964b5e7
Oliver Weissl <oliver.weissl@outlook.com>
2024-12-13 11:23:05 +0100

    Refractoring

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 """A collection of models used in the experiments."""
 
-from ._cl_trainer import ClTrainer, TrainConfig
 from ._generate_output import generate_output
 from ._load_stylegan import load_stylegan
 
-__all__ = ["generate_output", "ClTrainer", "TrainConfig", "load_stylegan"]
+__all__ = ["generate_output", "load_stylegan"]
